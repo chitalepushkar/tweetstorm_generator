48897d11c58dce050386b162610b4f99934047eb
chitalepushkar <chitalepushkar@gmail.com>
2020-09-27 20:31:15 -0400

    Update tweetstormgenerator.rb

--- tweetstormgenerator.rb
+++ tweetstormgenerator.rb
@@ -5,7 +5,7 @@
 MAX_WORD_COUNT = 137
 
 def create_tweetstorm(large_tweet)
-  # Checking for nil or whitespace text as an argument
+  # Checking for nil or whitespace text as an argument.
   if large_tweet.nil? || large_tweet =~ /\A\s*\z/
     print 'Argument missing. Please pass tweet text as an argument.'
     return
@@ -15,8 +15,10 @@ def create_tweetstorm(large_tweet)
   tweet = ''
   for char_index in 0..large_tweet.length-1
     if (tweet.length < MAX_WORD_COUNT)
+      # Append character to temporary string 'tweet' until length reaches MAX_WORD_COUNT.
       tweet << large_tweet[char_index]
     else
+      # Add tweet to collection if character limit reached.
       tweet_collection << tweet
       tweet = large_tweet[char_index]
     end
@@ -25,9 +27,9 @@ def create_tweetstorm(large_tweet)
   prefix_tweet_count(tweet_collection)
 end
 
-# Prefix (tweet number / total tweets) to individual tweets
+# Prefix (tweet number / total tweets) to individual tweets.
 def prefix_tweet_count(tweet_collection)
-  # Deleting tweets containing only whitespaces.
+  # Deleting tweet chunks containing only whitespaces.
   tweet_collection.delete_if {|tweet| tweet =~ /\A\s*\z/ }
   tweet_collection.each_with_index do |tweet, tweet_index|
     print tweet.prepend("#{tweet_index+1}/#{tweet_collection.length}")
